71b1b942774b1650d94a255171fe52d106aa63b2
ilya <lizunovilyastep@gmail.com>
2022-12-10 13:05:36 +0200

    добавлен метод is_alive

---
+++ character.py
@@ -0,0 +1,27 @@
+class Character:
+    name = ''
+    health = 100
+    damage = 1
+    defence = 0
+
+    def __init__(self, name, health=100, damage=1, defence=0):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
+
+    def __str__(self):
+        return f' == {self.name} ==\n' \
+            f' Здоровье: {self.health}\n' \
+            f' Урон: {self.damage}\n' \
+            f' Защита: {self.defence}\n'
+
+    def take_damage(self, damage):
+        self.health -= damage
+
+    def attack(self, target):
+        target.take_damage(self.damage)
+
+    def is_alife (self):
+        return self.health > 0
+
